6742c915fc8482e674507bd7c3fbcf4460b091fe
Nick Rafala <njrafala@hotmail.com>
2021-05-23 17:18:52 -0400

    Created UI

--- gamewindow.cpp
+++ gamewindow.cpp
@@ -15,6 +15,18 @@ GameWindow::GameWindow(QWidget *parent)
     viewModel.placeCell(Cell(0,-1));
     viewModel.printLiveCells();
 
+    for (int y = 0; y < 10; y++) {
+        for (int x = 0; x < 10; x++) {
+            QWidget *cell = new QWidget();
+            QString styleSheet = cell->styleSheet();
+            styleSheet += "background-color: #DCDCDC";
+            cell->setStyleSheet(styleSheet);
+            ui->board->addWidget(cell, x, y);
+        }
+    }
+
+
+
     int it = 0;
     while (it < 4) {
         viewModel.tick();
